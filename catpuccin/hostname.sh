4b0dcc04f22127f2fc6080f883e7ec88a195fa9d
Edder Rojas <edder.rojas@gmail.com>
2024-09-13 22:07:25 -0600

    fix: correct path for catpuccin component

--- catpuccin/hostname.sh
+++ catpuccin/hostname.sh
@@ -12,8 +12,7 @@ show_hostname() {
   color="$(get_tmux_option "@catppuccin_hostname_color" "$thm_magenta")"
   root_icon=$(get_tmux_batch_option "@catppuccin_hostname_root_icon" "󰏅")
   ssh_icon=$(get_tmux_batch_option "@catppuccin_hostname_ssh_icon" "")
-  # text="$(get_tmux_option "@catppuccin_hostname_text" "#(HOSTNAME_TMUX_ROOT_ICON=$root_icon HOSTNAME_TMUX_SSH_ICON=$ssh_icon \${TMUX_PLUGIN_MANAGER_PATH}/hostname-tmux/hostname.tmux)")"
-  text="$(get_tmux_option "@catppuccin_hostname_text" "#(HOSTNAME_TMUX_ROOT_ICON=$root_icon HOSTNAME_TMUX_SSH_ICON=$ssh_icon ~/Code/fun/hostname-tmux/hostname.tmux)")"
+  text="$(get_tmux_option "@catppuccin_hostname_text" "#(HOSTNAME_TMUX_ROOT_ICON=$root_icon HOSTNAME_TMUX_SSH_ICON=$ssh_icon \${TMUX_PLUGIN_MANAGER_PATH}/hostname-tmux/hostname.tmux)")"
 
   module=$(build_status_module "$index" "$icon" "$color" "$text")
 
